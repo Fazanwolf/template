a6165b4ae8166912b6f472be042ab100bd5f6d0a
Fazanwolf <fazanwolf@gmail.com>
2023-02-02 11:55:43 +0400

    [ADD] Basic template to workshop

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# template
+# workshop-bot-discord
+
+## Description
+
+Workshop to create a discord Bot.
+
+## Files
+
+- [Pré-requis](REQUIREMENTS.pdf)
+- [Sujet](SUBJECT.pdf)
+
+## Made by [Fazanwolf](https://github.com/Fazanwolf)
---
+++ src/bot.js
@@ -0,0 +1,76 @@
+//
+// ----------------------------
+// V1 - Import Packages and Modules
+// ----------------------------
+// Todo:
+// - Importer discord.js module
+// - Initialiser Dotenv
+//
+
+
+
+
+//
+// ----------------------------
+// V1 - SETUP DISCORD CLIENT
+// ----------------------------
+// Todo:
+// - Choisir les intentions (intents)
+//
+
+
+
+
+//
+// ----------------------------
+// SETUP DISCORD COMMANDS
+// ----------------------------
+// Todo:
+// V1:
+// - Charger les commandes
+// - Enregistrer les commandes sur l'API REST de Discord
+// V2:
+// - Création d'une collection slash dans client
+// - Créer la fonction registerSlashCommand
+// - Appeler la fonction dans rest.put
+//
+
+
+
+
+
+
+
+
+// ----------------------------
+// V2 - LOAD DISCORD INTERACTION
+// ----------------------------
+// Todo:
+// - Création des collections button, selection et modal dans client
+// - Créer la fonction registerInteraction
+// - Appeler la fonction registerInteraction
+//
+
+
+
+
+//
+// ----------------------------
+// LISTEN TO DISCORD EVENTS
+// ----------------------------
+// Todo:
+// V1:
+// - Event quand le bot est prêt
+// - Event quand une interaction est reçue
+// - Event quand un message est supprimé
+// V2:
+// - Créer la fonction registerEvents
+// - Charger les évènements avec registerEvents
+//
+
+
+
+
+
+
+
